d1f72142a0b5bba92d9541c0409ba6d5e083d479
SafeWinter <zandong_19@aliyun.com>
2024-09-27 00:25:35 +0800

    新增练习DIY（3.3小节数据绑定）：使用 margin-inline，async-await 语法糖，熟悉 v7.x 语法 join()

--- code/chapter_03/3.3-Binding_data/start/js/main.js
+++ code/chapter_03/3.3-Binding_data/start/js/main.js
@@ -1,34 +1,40 @@
-// Append a SVG container
-const svg = d3.select(".responsive-svg-container")
-  .append("svg")
-  .attr("viewBox", "0 0 1200 1600")
-  .style("border", "1px solid black");
+const svg = d3.select('.responsive-svg-container')
+    .append('svg')
+      .attr('viewBox', '0 0 1200 1600')
+      .style('border', '1px solid black');
+      
+const loadData = async () => {
+  const csvData = await d3.csv('../data/data.csv');
+  const data = csvData
+    // transform CSV data into array of objects
+    .map(({technology, count}) => ({
+      technology,
+      count: +count
+    }))
+    // order by count desc
+    .sort((a, b) => b.count - a.count);
 
-// Load, format and measure the dataset
-d3.csv("../data/data.csv", d => {
-  // Format the dataset
-  return {
-    technology: d.technology,
-    count: +d.count
-  };
-}).then(data => {
-  // Log the full dataset
-  console.log(data);
-
-  // How many rows the dataset contains
-  console.log(data.length);   // => 33
-
-  // Get the max and min values
-  console.log(d3.max(data, d => d.count));      // => 1078
-  console.log(d3.min(data, d => d.count));      // => 20
-  console.log(d3.extent(data, d => d.count));   // => [20, 1078]
-
-  // Sort the data in descending order
-  data.sort((a, b) => b.count - a.count);
-
-  // Pass the data to another function
   createViz(data);
-});
+};
 
-// Create the bar graph
-const createViz = (data) => {};
+/**
+ * Returns a color based on the technology
+ * @param {*} d - data object
+ * @returns {string} color
+ */
+const fillByTechFilter = ({technology: t}) => 
+    t === 'D3.js' ? 'yellowgreen' : 'skyblue'; 
+
+const createViz = (data) => {
+  const [barHeight, gap] = [20, 5];
+  svg.selectAll('rect')
+    .data(data)
+    .join('rect')
+      .attr('height', barHeight)
+      .attr('width', d => d.count)
+      .attr('x', 0)
+      .attr('y', (d, i) => i * (barHeight + gap))
+      .attr('fill', fillByTechFilter);
+};
+
+loadData();
