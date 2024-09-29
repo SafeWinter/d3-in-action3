cba4b19dd8f6f22b49a83b9437561517b2374223
SafeWinter <zandong_19@aliyun.com>
2024-09-30 00:19:50 +0800

    tested xScale with 4 data items from Fig-3.26; added icon

--- code/chapter_03/3.4-Scales/start/js/main.js
+++ code/chapter_03/3.4-Scales/start/js/main.js
@@ -18,15 +18,35 @@ const createViz = (data) => {
   const barHeight = 20;
   const gapSize = 5;
 
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.count)])
+    .range([0, 450]);
+
+  // testing the xScale function with test cases from fig. 3.26
+  testXScale(xScale);
+
   svg.selectAll('rect')
     .data(data)
     .join('rect')
       .attr('class', d => `bar bar-${d.technology}`)
       .attr('x', 0)
       .attr('y', (d, i) => (barHeight + gapSize) * i)
-      .attr('width', d => d.count)
+      .attr('width', d => xScale(d.count))
       .attr('height', barHeight)
       .style('fill', d => d.technology === 'D3.js' ? 'yellowgreen' : 'skyblue');
 };
+
+// testing the xScale function with test cases from fig. 3.26
+const testXScale = (xScale) => {
+  const tests = new Map([
+    [198,83], [414, 173], [852, 356], [1078, 450]
+  ]);
+  tests.forEach((expected, domain) => {
+    // const actual = xScale(domain).toFixed(0);
+    const actual = xScale(domain);
+    console.assert(actual === expected, 
+      `Expected ${expected}, but got ${actual} (${domain}, ${expected})`)
+  });
+}
 
 loadData();
